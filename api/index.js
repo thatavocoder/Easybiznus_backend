c9b9101d8b6306930cf573a7692c4099f70304be
Aashka Doshi <aashkadoshi7@gmail.com>
2025-05-30 01:02:33 +0530

    vercel build

--- api/index.js
+++
@@ -1,7 +0,0 @@
-// Explicitly require pg before other modules to ensure it's available
-require('pg');
-
-const app = require('../app');
-
-// Export the Express app for Vercel
-module.exports = app; 
